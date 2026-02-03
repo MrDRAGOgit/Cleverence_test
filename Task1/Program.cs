25b470af448e2c557786973b6989d5d084b6ff31
MrDRAGO <elitplaer@gmail.com>
2026-02-03 21:20:55 +0300

    Added comments to Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -9,16 +9,16 @@ namespace Task1
     class Program
     {
         private static String input_line;
-        private static string[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
 
         static void Main(string[] args)
         {
             
             while (true)
             {
+                Console.WriteLine("Please input your string, the programm will determine automatically if it requires compression or decompression:");
                 input_line = Console.ReadLine();
                 bool check = false;
-                foreach (char i in input_line)
+                foreach (char i in input_line) // Checking if line has any numbers, if it does its going to be a compressed string
                 {
                     if (Char.IsNumber(i))
                     {
@@ -28,13 +28,13 @@ namespace Task1
                 }
                 if (check)
                 {
-                    Console.WriteLine("Uncompressed line:");
+                    Console.WriteLine("Uncompressed line:"); // Uncompressing the string and outputting it
                     Uncompress();
                     Console.WriteLine(input_line);
                 }
                 else
                 {
-                    Console.WriteLine("Compressed line:");
+                    Console.WriteLine("Compressed line:"); // Compressing the string and outputting it
                     Compress();
                     Console.WriteLine(input_line);
                 }
@@ -43,23 +43,23 @@ namespace Task1
         }
         static void Compress()
         {
-            char[] split_string = input_line.ToCharArray();
+            char[] split_string = input_line.ToCharArray(); // Splitting the input string for ease of work
             var compressed_string = new StringBuilder(" ");
             int counter = 1;
             for(int i = 0; i < input_line.Length-1; i++)
             {
-                if (split_string[i] == split_string[i + 1])
+                if (split_string[i] == split_string[i + 1]) // Checking if there are same symbols going after one another and counting how many
                 {
                     counter++;
                 }
                 else
                 {
-                    if (counter > 1)
+                    if (counter > 1) // If there are more than one we write the amount than the symbol
                     {
                         String temp = counter.ToString() + split_string[i];
                         compressed_string.Append(temp);
                     }
-                    else
+                    else // If just one we write just the smybol
                     {
                         String temp = split_string[i].ToString();
                         compressed_string.Append(temp);
@@ -67,7 +67,7 @@ namespace Task1
                      counter = 1;
                 }
             }
-            if (counter > 1)
+            if (counter > 1) // Separate set of operations required for last symbol or group of symbols
             {
                 String temp = counter.ToString() + split_string[input_line.Length-1];
                 compressed_string.Append(temp);
@@ -82,19 +82,19 @@ namespace Task1
         }
         static void Uncompress()
         {
-            char[] split_string = input_line.ToCharArray();
+            char[] split_string = input_line.ToCharArray(); // Splitting the input string for ease of work
             var uncompressed_string = new StringBuilder(" ");
             for (int i = 0; i < input_line.Length - 1; i++)
             {
-                if (Char.IsNumber(split_string[i]))
+                if (Char.IsNumber(split_string[i])) // Checking if the symbol is a number or not
                 {
-                    for (int j = 0; j < Char.GetNumericValue(split_string[i]); j++)
+                    for (int j = 0; j < Char.GetNumericValue(split_string[i]); j++) // If it is than we add that many of the symbol that follows in the string
                     {
                         uncompressed_string.Append(split_string[i + 1]);
                     }
-                    i++;
+                    i++; // We make sure to skip the next symbol as it has been already used for the task
                 }
-                else
+                else // If it isnt a number and we havent skipped it that means we add as is
                 {
                     uncompressed_string.Append(split_string[i]);
                 }
